9b60dc181279ffc4410b8da04c617c3b06191741
Ahmed Rewainy <arewainy2@gmail.com>
2022-11-21 17:01:04 +0200

    Steup of heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const geocode = require('./utlis/geocode')
 const forrecast = require('./utlis/forrecast')
 
 const app = express()
+const port=process.env.PORT || 3000
+
 //define path
 const publicDirectory = path.join(__dirname, '../public')
 const viewsPath= path.join(__dirname, '../templates/views')
@@ -94,7 +96,7 @@ res.render('404', {
 })
 })
 
-app.listen(3000, ()=> {
-    console.log('Server is on port 3000')
+app.listen(port, ()=> {
+    console.log('Server is on port '+ port)
 
 })
